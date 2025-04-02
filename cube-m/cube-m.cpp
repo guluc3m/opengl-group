d3c5c632e1ffbdded7fa4c1dbaaeaf73345ad383
daniel <100499700@alumnos.uc3m.es>
2025-04-02 14:01:47 +0200

    Ahora pinta un cubo xd

--- cube-m/cube-m.cpp
+++ cube-m/cube-m.cpp
@@ -203,27 +203,6 @@ int main () {
 		// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 
-		//computeMatricesFromInputs();
-		glm::mat4 ProjectionMatrix = Projection;
-		glm::mat4 ViewMatrix = View;
-		glm::mat4 ModelMatrix = Model;
-		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-
-		glm::vec3 position = glm::vec3( 0, 0, 5 );
-		// horizontal angle : toward -Z
-		float horizontalAngle = 3.14f;
-		// vertical angle : 0, look at the horizon
-		float verticalAngle = 0.0f;
-		// Initial Field of View
-		float initialFoV = 45.0f;
-
-		float speed = 3.0f; // 3 units / second
-		float mouseSpeed = 0.005f;
-		
-		// Get mouse position
-		//int xpos, ypos;
-		//glfwGetC(&xpos, &ypos);
-
 
 			
 
